4a80934e580f3ba11669d53e24e259a6083c22d0
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-05-27 18:38:14 -0700

    added main menu prompts

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// load necessary modules
+// SPDX-License-Identifier: MIT
+// packages needed for this application
 const inquirer = require('inquirer');
 const mysql = require('mysql2')
 const table = require('console.table');
@@ -16,7 +17,7 @@ const db = mysql.createConnection(
       password: '',
       database: 'employee_tracker_db'
     },
-    console.log(`Connected to the classlist_db database.`)
+    console.log(`Connected to employee_tracker_db database.`)
   );
 
 // a function to do some ASCIII art :)
@@ -45,3 +46,57 @@ const tryAscii = () => {
 };
 
 tryAscii();
+
+// Main Menu prompts
+const mainMenu = [
+    {
+      name: "menuchoice",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add An Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add A Department",
+        "Quit",
+      ]
+    }
+  ];//end main menu
+
+// main function for app
+const doApp = async () => {
+    //let myInput;
+    do {
+      const response = await inquirer.prompt(mainMenu);
+        if (response.choices === 'View All Employees') {
+          // TODO: View All Employees
+          }
+        if (response.choices === 'Add An Employee') {
+          // TODO: Add An Employee
+          }
+        if (response.choices === 'Update Employee Role') {
+          // TODO: Update Employee Role
+          }
+        if (response.choices === 'View All Roles') {
+          // TODO: View All Roles
+          }
+        if (response.choices === 'Add Role') {
+          // TODO: Add Role
+          }
+        if (response.choices === 'View All Departments') {
+          // TODO: View All Departments
+          }
+        if (response.choices === 'Add A Department') {
+          // TODO: Add A Department
+          }
+        }
+        while (myInput !="Quit");
+  };// end of doApp()
+
+// ****
+// Start the app here
+// ****
+doApp();
